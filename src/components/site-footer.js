d8aa618350731e5151b6e674d1f20f08832a6053
Valen Munsil  <vmunsil@gmail.com>
2025-10-27 11:51:51 -0700

    Added the new navbar and footer to src components

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -2,24 +2,33 @@ class SiteFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
             <!-- Footer: single source of truth -->
-            <nav class="navbar fixed-bottom navbar-dark bg-dark">
-      <div class="container-fluid d-flex justify-content-around">
-        <!-- Home Button -->
-        <button class="btn btn-outline-light" onclick="history.back()">
-          <img src="./images/icon-back.png" alt="Back" class="icon" />
-        </button>
+            <footer>
+        <div class="navigation">
+          <img
+            id="back"
+            src="images/icon-back.PNG"
+            alt="Back"
+            width="100"
+            height="100"
+          />
 
-        <!-- Friends Button -->
-        <a href="friends.html" class="btn btn-outline-light">
-          <img src="./images/icon-friends.png" alt="Friends" class="icon" />
-        </a>
+          <img
+            id="home"
+            src="images/icon-home.PNG"
+            alt="Home"
+            width="100"
+            height="100"
+          />
 
-        <!-- Back Button -->
-        <a href="index.html" class="btn btn-outline-light">
-          <img src="./images/icon-home.jpg" alt="Home" class="icon" />
-        </a>
-      </div>
-    </nav>
+          <img
+            id="friends"
+            src="images/icon-friends.PNG"
+            alt="Friends"
+            width="100"
+            height="100"
+          />
+        </div></footer
+    >
         `;
   }
 }
