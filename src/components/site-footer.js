4e52d8ea0c689819c8e00ce0654908a5d9a1a998
Valen Munsil  <vmunsil@gmail.com>
2025-10-29 12:12:02 -0700

    fixed all merge conflicts

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -2,26 +2,6 @@ class SiteFooter extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
             <!-- Footer: single source of truth -->
-<<<<<<< HEAD
-            <nav class="navbar fixed-bottom navbar-dark bg-dark">
-      <div class="container-fluid d-flex justify-content-around">
-        <!-- Home Button -->
-        <button class="btn btn-outline-light" onclick="history.back()">
-          <img src="./images/icon-back.png" alt="Back" class="icon" />
-        </button>
-
-        <!-- Friends Button -->
-        <a href="friends.html" class="btn btn-outline-light">
-          <img src="./images/icon-friends.png" alt="Friends" class="icon" />
-        </a>
-
-        <!-- Back Button -->
-        <a href="index.html" class="btn btn-outline-light">
-          <img src="./images/icon-home.jpg" alt="Home" class="icon" />
-        </a>
-      </div>
-    </nav>
-=======
             <footer>
         <div class="navigation">
           <img
@@ -47,9 +27,7 @@ class SiteFooter extends HTMLElement {
             width="100"
             height="100"
           />
-        </div></footer
-    >
->>>>>>> 55770273086af3cbdd9b52ac6539a8172cef83c2
+        </div></footer>
         `;
   }
 }
