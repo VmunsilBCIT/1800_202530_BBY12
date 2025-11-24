122774446eb0b26817b69bf857c9113b0af5f05a
ndhtl <gotojpc@gmail.com>
2025-11-24 00:23:32 -0800

    Fixed the error that logout from profile page, it will jump between index.html and main.html, and fixed some pages didn't show favcon.

--- src/setting.js
+++ src/setting.js
@@ -1,22 +1,27 @@
 import { auth } from "./firebaseConfig.js";
 import { signOut } from "firebase/auth";
 
+// Language bar
 document.getElementById("lang").addEventListener("click", () => {
   window.location.href = "/language.html";
 });
 
+// Blacklist bar
 document.getElementById("block").addEventListener("click", () => {
   window.location.href = "/blocked-users.html";
 });
 
+// Do not distrub bar
 document.getElementById("dnd").addEventListener("click", () => {
   window.location.href = "/donotdisturb.html";
 });
 
+// User customer service bar
 document.getElementById("customer").addEventListener("click", () => {
   window.location.href = "/customer-service.html";
 });
 
+// Change of Email and Password bar
 document.getElementById("sns").addEventListener("click", () => {
   window.location.href = "/signinsecurity.html";
 });
@@ -27,7 +32,7 @@ document.getElementById("logoutBtn").addEventListener("click", () => {
     signOut(auth)
         localStorage.removeItem("loggedIn");
         console.log("User signed out successfully.");
-        window.location.href = "/login.html";
+        window.location.href = "login.html";
       }
   catch(error) {
     console.error("Error signing out:", error);
